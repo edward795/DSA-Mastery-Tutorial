51ac187552c0864dcc5f36e82af57ce89e469fe6
edward795 <ayyappadasgb777999@gmail.com>
2023-11-18 16:00:49 +0530

    #Trees : #14

---
+++ Trees/Binary_Search_Tree/FixBSTWith2NodesSwapped.java
@@ -0,0 +1,9 @@
+package Trees.Binary_Search_Tree;
+
+public class FixBSTWith2NodesSwapped {
+    public static void main(String[] args) {
+        Node root=Node.getBST();
+    }
+
+    static void
+}
