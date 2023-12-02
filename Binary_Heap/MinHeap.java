367af7bc024349e18583e24ebc2a98c4a84f7f86
edward795 <ayyappadasgb777999@gmail.com>
2023-12-02 13:59:11 +0530

    #Heap : #2

--- Binary_Heap/MinHeap.java
+++ Binary_Heap/MinHeap.java
@@ -14,4 +14,69 @@ public class MinHeap{
     int left(int i){ return (2*i+1); }
     int right(int i){ return (2*i+2); }
     int parent(int i){ return (i-1)/2; }
+
+    //TC : (logn)
+    void minHeapify(int i){
+        int lt=left(i);
+        int rt=right(i);
+        int smallest=i;
+
+        if(lt<size && arr[lt]<arr[i]) 
+            smallest=lt; 
+
+        if(rt<size && arr[rt]<arr[i])
+            smallest=rt; 
+
+        if(smallest!=i){
+            swap(arr,i,smallest);
+            minHeapify(smallest);
+        } 
+    }
+
+    void insert(int x){
+        if(size==capacity) return;
+        arr[size++]=x;
+
+        for(int i=size-1;i!=0 && arr[parent(i)]>arr[i];){
+            swap(arr,i,parent(i));
+            i=parent(i);
+        }
+    }
+
+    //TC : O(logn)
+    int extractMin(){
+        if(size==0) 
+            return Integer.MAX_VALUE;
+
+        if(size==1){
+            size--;
+            return arr[0];
+        }
+
+        swap(arr,0,size-1);
+        size--;
+        minHeapify(0);
+        return arr[size];
+    }
+
+    //TC : O(logn)
+    void decreaseKey(int i,int x){
+        arr[i]=x;
+        while(i!=0 && arr[parent(i)]>arr[i]){
+            swap(arr,i,parent(i));
+            i=parent(i);
+        }
+    }
+
+    //TC : O(logn)
+    void deleteKey(int i){
+        decreaseKey(i, Integer.MIN_VALUE);
+        extractMin();
+    }
+
+    static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
