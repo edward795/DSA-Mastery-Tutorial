03109ccdcccafcc1795a04fd94837066993b33ec
edward795 <ayyappadasgb777999@gmail.com>
2024-02-03 12:36:20 +0530

    #Greedy Algorithms-2

---
+++ Greedy_Algorithms/FractionalKnapsack.java
@@ -0,0 +1,38 @@
+package Greedy_Algorithms;
+import java.lang.Comparable;
+import java.util.Arrays;
+
+public class FractionalKnapsack {
+    public static void main(String[] args) {
+        Item[] items=new Item[]{new Item(10,200),new Item(5,50),new Item(20,100)};
+        System.out.println("Max KnapSack Capacity is : "+fracKnapSack(items, 15));
+    }
+
+    static double fracKnapSack(Item[] arr,int W){
+        Arrays.sort(arr);
+        double res=0.0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].wt<=W){
+                res=res+arr[i].value;
+                W=W-arr[i].wt;
+            }else{
+                res=res+arr[i].value*(double)W/(double)arr[i].wt;
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+
+class Item extends Comparable<Item>{
+    int wt,value;
+
+    Item(int wt,int value){
+        this.wt=wt;
+        this.value=value;
+    }
+    public int compareTo(Item i){
+        return this.wt*i.value-i.wt*this.value;
+    }
+}
---
+++ Greedy_Algorithms/JobSequencingProblem.java
@@ -0,0 +1,49 @@
+package Greedy_Algorithms;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class JobSequencingProblem {
+    public static void main(String[] args) {
+        Job[] jobs=new Job[]{new Job(0,2,50),new Job(1,2,60),new Job(2,3,20),new Job(3,3,30)};
+        System.out.println("Max Profit "+JobScheduling(jobs, 4));
+    }
+
+    public static int[] JobScheduling(Job[] arr,int n){
+        Arrays.sort(arr,new MyCmp());
+        int[] res=new int[]{0,0};
+        int maxDeadline=0;
+        for(int i=0;i<n;i++){
+            maxDeadline=Math.max(maxDeadline,arr[i].deadline);
+        }
+
+        int[] visited=new int[maxDeadline];
+        Arrays.fill(visited,-1);
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].deadline-1;j>=0;j--){
+                if(visited[j]==-1){
+                    visited[j]=i;
+                    res[0]++;
+                    res[i]=arr[i].profit;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Job{
+    int id,profit,deadline;
+
+    Job(int id,int profit,int deadline){
+        this.id=id;
+        this.profit=profit;
+        this.deadline=deadline;
+    }
+}
+class MyCmp implements Comparator<Job>{
+    public int compare(Job j1,Job j2){
+        return j2.profit-j1.profit;
+    }
+}
